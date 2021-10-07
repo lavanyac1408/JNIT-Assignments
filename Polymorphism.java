c390c5594de43afe7e6af191f599ad4a9fd37bd2
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-10-06 22:47:53 -0500

    Polymorphism

---
+++ Polymorphism.java
@@ -0,0 +1,22 @@
+class Restuarant {
+	void takeOrder(String order) {
+	System.out.println("Order given is "+order);
+}
+	void takeOrder(String order, String order1) {
+	System.out.println("Order given is "+order+" and "+order1);
+}
+	void takeOrder(String order, int sno) {
+	System.out.println("Order given is "+order+" and "+sno);
+}
+}
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		Restuarant ohris=new Restuarant();
+		ohris.takeOrder("Idly");
+		ohris.takeOrder("Dosa",3);
+		ohris.takeOrder("Poori","Dosa");
+	}
+}
+
