87146dc52c1b91e0eb5e0c6cf8b38d16336e71a8
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-10-21 22:32:09 -0500

    IfElseStatement

---
+++ IfElseStatement1.java
@@ -0,0 +1,13 @@
+
+public class IfElseStatement1 {
+
+	public static void main(String[] args) {
+		String residingstate = "Texas" ;
+		
+		if (residingstate == "California")
+			System.out.println("Eligible for Texas State Form of WorkersComp");
+		else
+			System.out.println("Eligible for Form of WorkersComp");
+	}
+
+}
