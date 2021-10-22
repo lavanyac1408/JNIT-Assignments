f7d586ed8608c0f3a5537fe0cc3e56d9058638dd
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-10-21 22:32:54 -0500

    NestedIfElseStatement

---
+++ NestedIfElseStatement.java
@@ -0,0 +1,19 @@
+
+public class NestedIfElseStatement {
+
+	public static void main(String[] args) {
+		int a=500;
+		if (a>0) {
+			if(a>1000)
+				System.out.println("a is +ve and is >1000");
+			else
+				System.out.println("a is +ve and a is <1000");
+		}
+		else {
+			System.out.println("a is negative");
+		}
+		
+
+	}
+
+}
