ea7fa1383702261d072203aa197be691ad59d314
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-10-27 22:28:06 -0500

    WhileLoopOperators

---
+++ WhileLoopMultiplicationTable.java
@@ -0,0 +1,14 @@
+
+public class WhileLoopMultiplicationTable {
+
+	public static void main(String[] args) {
+		int n=5,i=1,multiply=5;
+		while(i<=10) {
+			multiply=n*i;
+			
+			System.out.println( n +"*" + i + "=" + multiply);
+			i++;
+			}
+		
+		}
+}
