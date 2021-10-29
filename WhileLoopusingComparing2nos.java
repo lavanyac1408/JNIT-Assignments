21482a784dd4e04342b1f4050a2d3668d447f8a9
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-10-29 08:47:39 -0500

    WhileLoopwithArithmeticOperators

---
+++ WhileLoopusingComparing2nos.java
@@ -0,0 +1,17 @@
+
+public class WhileLoopusingComparing2nos {
+
+	public static void main(String[] args) {
+		int large =2345;
+		int small =3;
+		
+		while(large>small) {
+			System.out.println("Large = "+ large+ " and " + "Small = " +small);
+			large = large /2;
+			small =small *2;
+			
+		}
+
+	}
+
+}
