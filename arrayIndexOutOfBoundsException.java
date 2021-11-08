b9f04f4c6ee3c224f0317e9076334515ab9a0c71
lavanyac1408 <91925716+lavanyac1408@users.noreply.github.com>
2021-11-08 08:52:04 -0600

    Runtime Exceptions

---
+++ arrayIndexOutOfBoundsException.java
@@ -0,0 +1,15 @@
+
+public class arrayIndexOutOfBoundsException {
+
+	public static void main(String[] args) {
+		try {
+			int a [] = new int [10]; // array with only 10 elements
+			
+			a[11] = 9;
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBounds");
+		}
+	}
+
+}
